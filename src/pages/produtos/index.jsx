08e1bd5657e8f31247068de49fc470d2b2ce732a
hannafreitasc <hannafreitascc@gmail.com>
2025-06-09 12:04:01 -0300

    pagina-simples-token

---
+++ src/pages/produtos/index.jsx
@@ -0,0 +1,9 @@
+import PageWrapper from "@/components/PageWrapper"
+
+export default function Produtos(){
+    return(
+        <PageWrapper>
+            <p>Olá mundo</p>
+        </PageWrapper>
+    )
+}
